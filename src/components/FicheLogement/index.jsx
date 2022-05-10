01ee405916939b2becabf87a88d521d1888a113d
jsr029 <jsr059@gmail.com>
2022-05-10 12:30:03 +0200

    Update

--- src/components/FicheLogement/index.jsx
+++ src/components/FicheLogement/index.jsx
@@ -22,17 +22,39 @@ function FicheLogement() {
 			<div className='lgt-fiche'>						
 					{data && data.length && data.map((d,a) =>
 					(urlId === d.id) ? (
+						<>
 							<div className='lgt-fiche-underImg-bloc' key={a+4}>
-								<div className='lgt-fiche-title'>
-									{d.title}
+								<div className='lgt-fiche-title-bloc'>
+									<div className='lgt-fiche-title'>
+										{d.title}
+									</div>
+									<div className='lgt-fiche-location'>
+										{d.location}
+									</div>
+								</div>
+								<div className='lgt-fiche-host-bloc'>
+									<div className='lgt-fiche-host'>
+										<div className='lgt-host-name'>
+											{d.host.name}
+										</div>
+										<div className='lgt-host-pict'>
+											<img src={d.host.picture} alt={'Photo de profil de : '+d.host.name} />
+										</div>
+									</div>
+								</div>									
+							</div>
+							<div className='lgt-fiche-tagsRatingBloc' key={a+5}>
+								<div className='lgt-fiche-tags'>
+									{d.tags.map(t => <span>{t}</span> )}
 								</div>
 								<div className='lgt-fiche-rating'>
-									<span>Rating : {d.rating}</span>
+									{d.rating}
 								</div>
 							</div>
+						</>
 						) : ''
 					)}
-				</div>
+			</div>
 		</>
 	)
 
