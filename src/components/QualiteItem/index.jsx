df52c2c93299c0002928afc522627b2a4520e0fc
jsr029 <jsr059@gmail>
2022-05-14 14:44:55 +0200

    Update

--- src/components/QualiteItem/index.jsx
+++ src/components/QualiteItem/index.jsx
@@ -1,17 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import SlideToggle from 'react-slide-toggle';
 
 function QualiteItem({ id, name, description }) {
+   const [nam, setName] = useState(false)
+function clickHandler(){
+     setName((prevState) => (!prevState ));
+}
     return (
         <>
-            <h2>
-                <span className='lgt-fiche-left' key={id}>{name}</span>
-                <span className='lgt-fiche-right'>
-                    <img src='./img/arrowUpDown.png' alt='' className='lgt-fiche-hat' />
-                </span>
-            </h2>
-            <p>
-                {description}
-            </p>
+        <SlideToggle 
+            duration={800}
+            collapsed={true}
+                >
+            {({ toggle, setCollapsibleElement }) => (
+				<div className="my-collapsible">
+                    <h2>
+                        <span className='lgt-fiche-left' key={id}>{name}</span>
+                        <span className='lgt-fiche-right' onClick={toggle}>
+                            <img src='./img/arrowUpDown.png' alt='' className={'lgt-fiche-hat ' + (nam ? 'up' : 'down')} onClick={clickHandler}/>
+                        </span>
+                    </h2>
+					<div className="my-collapsible__content" ref={setCollapsibleElement}>
+						<p className="my-collapsible__content-inner">{description}</p>
+                    </div>
+                </div>
+                )}
+        </SlideToggle>
         </>
     );
 };
