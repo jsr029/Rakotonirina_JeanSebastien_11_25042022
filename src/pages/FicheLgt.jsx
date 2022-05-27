b191a930850208da5283a33369fe995b4e6230f3
jsr029 <jsr059@gmail.com>
2022-05-27 17:10:00 +0200

    Up

--- src/pages/FicheLgt.jsx
+++ src/pages/FicheLgt.jsx
@@ -2,8 +2,12 @@ import React from "react";
 import Banner from '../components/Banner'
 import FicheLogement from '../components/FicheLogement'
 import Footer from '../components/Footer'
+import { useTitle } from '../components/Utils/useTitle'
 
-function FicheLgt(props) {  
+function FicheLgt(props) { 
+    const url = window.location
+    const titre = url.split('/')[5]
+    useTitle("Kasa | Fiche Logement "+ titre) 
     return(
         <React.Fragment>
             <Banner />
