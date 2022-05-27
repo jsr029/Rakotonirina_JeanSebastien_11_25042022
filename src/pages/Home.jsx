261efa1be1094fd2f83cc624900f57716c81ccfd
jsr029 <jsr059@gmail.com>
2022-05-27 17:02:47 +0200

    Up

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,9 +3,10 @@ import Banner from '../components/Banner';
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import LogementList from '../components/LogementList';
+import { useTitle } from '../components/Utils/useTitle';
 
 const Home = (props) => {
-
+	useTitle("Kasa | Home")
 	return (
 		<React.Fragment>
 			<Banner />
